7b16f57a2d75d8da3c78ea2808471754b62a67d1
nicbuitr <n.buitrago29@uniandes.edu.co>
2017-03-28 20:26:07 -0500

    Format and Indentation

--- imports/ui/components/Fixtures.jsx
+++ imports/ui/components/Fixtures.jsx
@@ -2,105 +2,75 @@ import React, {Component, PropTypes} from "react";
 import {Meteor} from "meteor/meteor";
 import {Pollas} from "../../api/Pollas.js";
 
-
 export default class Fixtures extends Component {
 
   apostar() {
+    var pollas = new Object();
 
+    pollas.name = this.props.name;
+    pollas.visitante  = this.refs.visitante.value;
+    pollas.local = this.refs.local.value;
+    var jsonString= JSON.stringify(pollas);
+    console.log(pollas);
 
-   var pollas = new Object();
-  pollas.name = this.props.name;
-  pollas.visitante  = this.refs.visitante.value;
-  pollas.local = this.refs.local.value;
-  var jsonString= JSON.stringify(pollas);
-  console.log(pollas);
-
-    var user = Meteor.call('Pollas.agregarPolla',jsonString);
-    console.log('siretorno:',user);
-
-	}
-
-
-    render(){
-      if (this.props.fixture.status ==="FINISHED")
-      {
-        return (
-
-        <table className="fixes">
-        <thead>
-         <tr>
-             <th>Local</th>
-             <th>Visitante</th>
-             <th>Fecha</th>
-             <th>Jornada</th>
-             <th></th>
-
-
-
-         </tr>
-        </thead>
-        <tbody>
-         <tr>
-             <td>{this.props.fixture.homeTeamName}</td>
-             <td>{this.props.fixture.awayTeamName}</td>
-             <td>{this.props.fixture.date}</td>
-             <td>{this.props.fixture.matchday}</td>
-
-         </tr>
-
-        </tbody>
-
-        </table>
-  );
-      }
-      else {
-
-      return (
-                <table className="fixes">
-                <thead>
-                 <tr>
-                     <th>Local</th>
-                     <th>Visitante</th>
-                     <th>Fecha</th>
-                     <th>Jornada</th>
-                     <th>Goles Local</th>
-                     <th>Goles Visitante</th>
-                     <th> </th>
-
-                 </tr>
-             </thead>
-             <tbody>
-                 <tr>
-                     <td>{this.props.fixture.homeTeamName}</td>
-                     <td>{this.props.fixture.awayTeamName}</td>
-                     <td>{this.props.fixture.date}</td>
-                     <td>{this.props.fixture.matchday}</td>
-                    <td> <input type="number" ref="visitante" /></td>
-                    <td> <input type="number" ref="local" /></td>
-  <td>
-                     <button onClick={this.apostar.bind(this)}>Apostar</button>
-  </td>
-  <td>
-                     <button onClick={this.apostar.bind(this)}>SHARE</button>
-  </td>
-
-
-                 </tr>
-
-             </tbody>
-
-          </table>
-          );
-        }
-
-
-
-
-
-
-
-
-
-
-    }
+    var user = Meteor.call('Pollas.agregarPolla', jsonString);
+    console.log('siretorno:', user);
   }
+
+  render() {
+    if (this.props.fixture.status === "FINISHED")
+    {
+      return (
+              <table className="fixes">
+                  <thead>
+                    <tr>
+                      <th>Local</th>
+                      <th>Visitante</th>
+                      <th>Fecha</th>
+                      <th>Jornada</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                <tbody>
+                  <tr>
+                    <td>{this.props.fixture.homeTeamName}</td>
+                    <td>{this.props.fixture.awayTeamName}</td>
+                    <td>{this.props.fixture.date}</td>
+                    <td>{this.props.fixture.matchday}</td>
+                  </tr>
+                </tbody>
+
+              </table>
+              );
+    }
+    else {
+      return (
+              <table className="fixes">
+                <thead>
+                  <tr>
+                    <th>Local</th>
+                    <th>Visitante</th>
+                    <th>Fecha</th>
+                    <th>Jornada</th>
+                    <th>Goles Local</th>
+                    <th>Goles Visitante</th>
+                    <th> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{this.props.fixture.homeTeamName}</td>
+                    <td>{this.props.fixture.awayTeamName}</td>
+                    <td>{this.props.fixture.date}</td>
+                    <td>{this.props.fixture.matchday}</td>
+                    <td><input type="number" ref="visitante" /></td>
+                    <td><input type="number" ref="local" /></td>
+                    <td><button onClick={this.apostar.bind(this)}>Apostar</button></td>
+                    <td><button onClick={this.apostar.bind(this)}>SHARE</button></td>
+                  </tr>
+                </tbody>
+              </table>
+              );
+          }
+      }
+}
